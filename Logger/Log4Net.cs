756ee35b5e326bd29ecd02f849457c7c6ebfb90a
nodadamanelisi <nodadamanelisi@outlook.com>
2016-08-14 23:39:25 +0200

    Develop: added log4net.

---
+++ Logger/Log4Net.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    public static class Log4Net
+    {
+        public static void Log(string inputType, Exception ex)
+        {
+            SetUp.Setup();
+            
+            log4net.ILog log = log4net.LogManager.GetLogger(inputType);
+            log.Info("Testing message.");
+            log.Error("InnerException: " +ex.InnerException);
+            log.Error("Source: "+ex.Source);
+            log.Error("StackTrace: " + ex.StackTrace);
+        }
+    }
+}
